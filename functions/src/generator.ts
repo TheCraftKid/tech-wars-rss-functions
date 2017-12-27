d7e3ad6bb4ef445832a4403ace0edef266caf755
Willie Chalmers III <willieechalmers@gmail.com>
2017-12-27 12:29:26 -0600

    Refactor lengthy function params into interfaces

--- functions/src/generator.ts
+++ functions/src/generator.ts
@@ -1,10 +1,77 @@
 /**
  * Generates an RSS snippet for a podcast for Google Play Music.
+ */
+function generatePodcast(podcast: Podcast) {
+  return `<title>${podcast.title}</title>
+          <googleplay:author>${podcast.author}</googleplay:author>
+          <googleplay:email>${podcast.email}</googleplay:email>
+          <googleplay:image>${podcast.image}</googleplay:image>
+          <googleplay:description>${podcast.description}</googleplay:description>
+          <link>${podcast.link}</link>
+          <googleplay:explicit>${podcast.explicit ? 'true' : 'false'}</googleplay:explicit>`
+}
+
+/**
+ * Generates an RSS snippet for an episode of a podcast with the given information.
+ */
+function generateEpisode(episode: PodcastEpisode) {
+  return `<item>${episode.item}</item>
+          <title>${episode.title}</title>
+          <enclosure url="${episode.enclosure.url}" length="${episode.enclosure.length}
+                  type="${episode.enclosure.type}">
+          </enclosure>
+          <googleplay:description>${episode.description}</googleplay:description>
+          <itunes:summary>${episode.description}</itunes:summary>
+          <description>${episode.description}</description>
+          <guid>${episode.guid}</guid>
+          <pubDate>${episode.pubDate}</pubDate>
+          <googleplay:explicit>${episode.explicit ? 'Yes' : 'No'}</googleplay:explicit>
+          <itunes:explicit>${episode.explicit ? 'yes' : 'no'}</itunes:explicit>
+          <itunes:order>${episode.order}</itunes:order>
+          <googleplay:block>${episode.block ? 'yes' : 'no'}</googleplay:block>
+          <itunes:block>${episode.block ? 'yes' : 'no'}</itunes:block>`;
+
+}
+
+/**
+ * A container
+ */
+interface Enclosure {
+  url: string,
+  length: number,
+  type: string,
+}
+
+/**
+ * The subject of a {@link Podcast}.
+ */
+enum Category {
+  'Arts',
+  'Business',
+  'Comedy',
+  'Education',
+  'Games & Hobbies',
+  'Government & Organizations',
+  'Health',
+  'Kids & Family',
+  'Music',
+  'News & Politics',
+  'Religion & Spirituality',
+  'Science & Medicine',
+  'Society & Culture',
+  'Sports & Recreation',
+  'TV & Film',
+  'Technology',
+}
+
+/**
+ * A podcast that is associated with some {@link PodcastEpisode}s, built for 
+ * Google Play and iTunes distribution.
  * 
  * @param title {string} Name of the podcast
  * @param author {string} Author of your podcast (up to 255 characters)
  * @param email {string} Email address to authenticate podcast ownership 
- *                           (up to 255 characters)
+ *                       (up to 255 characters)
  * @param image {string} Image shown on Google Play Music to identify your 
  *                       podcast (URL can include up to 2048 characters).
  *                       The image needs to be: Square, minimum of 600 x 600
@@ -14,33 +81,34 @@
  *                       Note: A podcast needs an image at least 1200 x 1200 
  *                       pixels in size to be eligible for featuring. 
  * @param description {string} Description of your podcast (up to 
- *                            4000 characters)
+ *                             4000 characters)
  * @param language {string} Language used in the podcast: Language values from 
  *                          the ISO 639 list (two-letters, with some possible 
  *                          modifiers like "en-us") can be used.
  * @param link {string?} Link to an external page for your podcast
  * @param explicit {boolean?} Indicates if your podcast includes explicit content
  * @param category {string?} One of: Arts, Business, Comedy, Education, 
- *                          Games & Hobbies, Government & Organizations, Health,
- *                          Kids & Family, Music, News & Politics, 
- *                          Religion & Spirituality, Science & Medicine, 
- *                          Society & Culture, Sports & Recreation, TV & Film, 
- *                          Technology
+ *                           Games & Hobbies, Government & Organizations, Health,
+ *                           Kids & Family, Music, News & Politics, 
+ *                           Religion & Spirituality, Science & Medicine, 
+ *                           Society & Culture, Sports & Recreation, TV & Film, 
+ *                           Technology
  */
-function generatePodcast(title: string, author: string, email: string,
-  image: string, description: string, language: string, link?: string,
-  explicit?: boolean, category?: string) {
-  return `<title>${title}</title>
-          <googleplay:author>${author}</googleplay:author>
-          <googleplay:email>${email}</googleplay:email>
-          <googleplay:image>${image}</googleplay:image>
-          <googleplay:description>${description}</googleplay:description>
-          <link>${link}</link>
-          <googleplay:explicit>${ explicit ? 'true' : 'false'}</googleplay:explicit>`
+interface Podcast {
+  title: string,
+  author: string,
+  email: string,
+  image: string,
+  description: string,
+  language: string,
+  link?: string,
+  explicit?: boolean,
+  category?: Category,
 }
 
 /**
- * Generates an RSS snippet for an episode of a podcast with the given information.
+ * An episode of a {@link Podcast}, built for Google Play and 
+ * iTunes distribution.
  * 
  * @param item {string[]} Tags data for single episode - at least one complete 
  *                        <item> tag needs to be included in a feed.
@@ -61,28 +129,14 @@ function generatePodcast(title: string, author: string, email: string,
  * @param block {boolean?} Indicates an episode should be blocked from being
  *                         published on Google Play.
  */
-function generateEpisode(item: string[], title: string, enclosure: Enclosure,
-  description: string, guid?: string, pubDate?: string,
-  explicit: boolean = false, order?: number, block?: boolean) {
-  return `<item>${item}</item>
-          <title>${title}</title>
-          <enclosure url="${enclosure.url}" length="${enclosure.length}
-                  type="${enclosure.type}">
-          </enclosure>
-          <googleplay:description>${description}</googleplay:description>
-          <itunes:summary>${description}</itunes:summary>
-          <description>${description}</description>
-          <guid>${guid}</guid>
-          <googleplay:explicit>${explicit ? 'Yes' : 'No'}</googleplay:explicit>
-          <itunes:explicit>${explicit ? 'yes' : 'no'}</itunes:explicit>
-          <itunes:order>${order}</itunes:order>
-          <googleplay:block>${block ? 'yes' : 'no'}</googleplay:block>
-          <itunes:block>${block ? 'yes' : 'no'}</itunes:block>`;
-
-}
-
-interface Enclosure {
-  url: string,
-  length: number,
-  type: string,
+interface PodcastEpisode {
+  item: string[],
+  title: string,
+  enclosure: Enclosure,
+  description: string,
+  guid?: string,
+  pubDate?: string,
+  explicit?: boolean,
+  order?: number,
+  block?: boolean,
 }
