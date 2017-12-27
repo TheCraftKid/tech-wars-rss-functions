88b70750330a9bab5317fed65cacee23c571ae06
Willie Chalmers III <willieechalmers@gmail.com>
2017-12-27 13:30:37 -0600

    Finish index.ts  - Add handling for getRSSFeed function  - Make res.header code style consistent  - Follow TSLint module recommendation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,25 @@
 import * as functions from 'firebase-functions';
-import { Request } from 'express';
-import { Response } from 'express-serve-static-core';
+import { Request, Response } from 'express';
+import { PodcastManager } from './manager';
 
-export const getRSSFeed = functions.https.onRequest(
-  (req: Request, res: Response) => {
-
-
+const getRSSFeed = functions.https.onRequest(
+  async (req: Request, res: Response) => {
+    try {
+      const rss = await new PodcastManager().fetchPodasts();
+      // Poor little application/rss+xml; text/xml is de facto standard
+      res.header('Content-Type', 'text/xml; charset=UTF-8'); 
+      res.status(200).send(rss);
+    } catch (err) {
+      console.error(`Couldn't fetch RSS feed`, err);
+      res.header('Content-Type', 'text/plain');
+      res.status(500).send('Could not fetch RSS');
+    }
   });
 
-export const updateRSSFeed = functions.https.onRequest(
+const updateRSSFeed = functions.https.onRequest(
   (req: Request, res: Response) => {
     if (!req.is('application/json')) {
-      res.header('Allow: ')
+      res.header('Allow', 'application/json');
       res.status(400).send('Only JSON requests are accepted.');
     }
     res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
@@ -19,8 +27,13 @@ export const updateRSSFeed = functions.https.onRequest(
       case 'POST':
         break;
       default:
-        res.header('Allow: POST');
+        res.header('Allow', 'POST');
         res.send(405).send('Only POST requests are accepted.');
         break;
     }
   });
+
+export {
+  getRSSFeed,
+  updateRSSFeed,
+}
