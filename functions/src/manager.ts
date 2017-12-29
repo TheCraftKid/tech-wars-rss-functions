0a1b3bfb09eb9657587e88a0b25638a264561228
Willie Chalmers III <willieechalmers@gmail.com>
2017-12-28 23:59:14 -0600

    Start work on manager to handle podcast CRUD operations

--- functions/src/manager.ts
+++ functions/src/manager.ts
@@ -1,3 +1,29 @@
-class PodcastManager {
-  private podcasts = [];
+import { PodcastEpisode, Podcast } from "./generator";
+import { fetchRSS } from "./fetcher";
+
+/**
+ * Utility class that handles creation, reading, updating, and deletion of
+ * {@link PodcastEpisode}s.
+ */
+export class PodcastManager {
+
+  public createPodcast(podcast: Podcast) {
+    // TODO
+  }
+
+  /**
+   * Appends the given episode to the podcast's RSS feed. 
+   * 
+   * @param episode {PodcastEpisode} The episode to 
+   */
+  public updatePodcast(episode: PodcastEpisode) {
+    const rss = fetchRSS()
+  }
+
+  /**
+   * Fetches the podcasts from 
+   */
+  public async fetchPodasts(): Promise<string> {
+    return await fetchRSS();
+  }
 }
